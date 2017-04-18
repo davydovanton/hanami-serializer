a3b1f2a7cead4dd1fdaa68b984f0602ce8a002d9
Anton Davydov <antondavydov.o@gmail.com>
2017-04-18 16:48:03 +0300

    Initialize

---
+++ lib/hanami/serializer.rb
@@ -0,0 +1,7 @@
+require "hanami/serializer/version"
+
+module Hanami
+  module Serializer
+    # Your code goes here...
+  end
+end
---
+++ lib/hanami/serializer/version.rb
@@ -0,0 +1,5 @@
+module Hanami
+  module Serializer
+    VERSION = "0.1.0"
+  end
+end
